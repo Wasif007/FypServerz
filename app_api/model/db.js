8d108c863dad77e4b768b908002b6f48874d1472
Wasif Ateeq <wasifateeq007@hotmail.com>
2017-02-02 03:15:40 -0800

    ch

--- app_api/model/db.js
+++ app_api/model/db.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var gracefulShutdown;
 var dbURI = 'mongodb://localhost/FypServer';
 if (process.env.NODE_ENV === 'production') {
-    dbURI = process.env.MONGOLAB_URI;
+    dbURI = process.env.MONGOLAB_CHARCOAL_URI;
 }
 
 mongoose.connect(dbURI);
