8d3505fd18c96c10ae9ac8cbd6049f1ff881b34b
Wasif Ateeq <wasifateeq007@hotmail.com>
2017-02-03 17:18:13 -0800

    changes made

--- app_api/controller/supervisorValidation.js
+++ app_api/controller/supervisorValidation.js
@@ -2,6 +2,24 @@ var mongoose=require('mongoose');
 var SupervisorValidation=mongoose.model('SupervisorValidation');
 var passport=require('passport');
 
+var LocalStrategy = require('passport-local').Strategy;
+passport.use(new LocalStrategy({
+usernameField: 'email'
+},
+  function(username, password, done) {
+    SupervisorValidation.findOne({ email: username }, 
+      function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
 var sendJSONresponse = function(res, status, content) {
   res.status(status);
   res.json(content);
