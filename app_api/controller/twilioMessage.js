c36bf6952cf82387da5857cdabc5633fb932ad54
Wasif Ateeq <wasifateeq007@hotmail.com>
2017-02-05 04:34:44 -0800

    Twilio configured

--- app_api/controller/twilioMessage.js
+++ app_api/controller/twilioMessage.js
@@ -1,9 +1,21 @@
 var client = require('twilio')('AC678ea2ec1d3504e754eaeffce34b6bea', '22c8728b18ee523c272c4efc44919e04');
+var sendJSONresponse = function(res, status, content) {
+  res.status(status);
+  res.json(content);
+};
 
 module.exports.sendingMessage = function(req, res) {
  
+if(!req.body.code || !req.body.to)
+{
+sendJSONresponse(res,404,{
+  "Message":"Code and Phone Number required"
+});
+return;
+
+}
   client.messages.create({
-    body: "This is the ship that made the Kessel Run in fourteen parsecs?", 
+    body: req.body.code, 
     to: req.body.to,
     from: '+15124025641'
     // mediaUrl: 'http://www.yourserver.com/someimage.png'
@@ -11,8 +23,16 @@ module.exports.sendingMessage = function(req, res) {
     if (err) {
       console.error('Could not notify administrator');
       console.error(err);
-    } else {
-      console.log('Administrator notified');
+      sendJSONresponse(res,404,{
+        "Message":err
+      });
+      return;
+
+    } 
+    else {
+      sendJSONresponse(res,200,{
+        "Message":"Administrator notified"
+      });
     }
   });
 };
